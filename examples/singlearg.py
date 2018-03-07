e3f1d3fea47adb609ba1a8bee67e3739e8519ecd
Colten Jackson <colten.jackson@annalect.com>
2018-03-07 12:16:03 -0500

    singlearg

--- examples/singlearg.py
+++ examples/singlearg.py
@@ -7,33 +7,29 @@ import StringIO, time, os
 import pyvalidate
 
 valid = pyvalidate.parameters({
-    "mysql": "labsdb",
-    "required": {
-        "arg": {
-            "type":"number::int",
-            "info":"number of rows and columns",
-            "placeholder":"for example: 3",
-            "verify":"^\d+$"
-        },
-        "s3upload": {
-            "info": "Where should the result be saved?",
-            "type": "text::s3",
-            "value": 'example' + str(int(time.time())) + '.csv',
-            "verify": "^[A-Za-z0-9._]+$" # restrict filename if you want
-        }
+    "database": {
+        "mysql": "labsdb",
     },
-    "optional": {
-        "labels": {
-            "type":"text::bool",
-            "info":"Whether to print row and column labels",
-            "verify":"(?i)^(true|false)$",
-            "placeholder": "True or False"
-        }
+    "arg": {
+        "type":"number::int",
+        "info":"number of rows and columns",
+        "placeholder":"for example: 3",
+        "verify":"^\d+$"
+    },
+    "s3upload": {
+        "info": "Where should the result be saved?",
+        "type": "text::s3",
+        "value": 'example' + str(int(time.time())) + '.csv',
+        "verify": "^[A-Za-z0-9._]+$" # restrict filename if you want
+    }
+    "labels": {
+        "type":"text::bool",
+        "info":"Whether to print row and column labels",
+        "verify":"(?i)^(true|false)$",
+        "placeholder": "True or False"
     }
 })
 # Guassian distribution
 samples = pandas.DataFrame(numpy.random.rand(valid.rows, valid.cols))
 valid.s3upload.put(Body=samples.to_csv())
-
-fulls3path = '/id/%s/%s' % (os.environ.get('USER', 'undefined'), valid.args['required']['s3Object']['value'])
-valid.output({'downloads3': fulls3path})
+valid.output({'downloads3': valid.s3upload.pathname})
